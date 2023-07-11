0080618e61b53c16c9642de7503d7dfcc60983fc
Kykim0818 <Kykim@seegene.com>
2023-07-11 19:42:52 +0900

    make input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,30 @@
+import { HTMLAttributes } from "react";
+import styled from "styled-components";
+
+interface Props extends HTMLAttributes<HTMLInputElement> {
+  readonly?: boolean;
+}
+
+export const Input = ({ readonly, ...props }: Props) => {
+  return <StyledInput readOnly={readonly} {...props} />;
+};
+
+const StyledInput = styled.input`
+  box-sizing: border-box;
+  padding: 10px 20px;
+  border-radius: 34px;
+  border: 1px solid var(--color-bg, #f6f8fc);
+
+  font-size: 14px;
+  font-weight: 400;
+  line-height: normal;
+
+  &:focus {
+    outline: none;
+    border: 1px solid var(--color-main, #009eb2);
+  }
+
+  &::placeholder {
+    color: #acb1c6;
+  }
+`;
