7106458670c1e9a6217edb91d59ded36d2786e56
Kykim0818 <Kykim@seegene.com>
2023-07-28 17:31:46 +0900

    골프장 추가

--- src/pages/MakeGame/MakeGolfCourse/MakeGolfCourse.tsx
+++ src/pages/MakeGame/MakeGolfCourse/MakeGolfCourse.tsx
@@ -1,20 +1,27 @@
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import Button from "../../../components/Button";
 import TitleAsset from "../../../components/TitleAsset";
 
 export const MakeGolfCourse = () => {
   const navigate = useNavigate();
+
   return (
-    <div>
+    <Styled.Wrapper>
       <TitleAsset
         title="골프장 추가"
         visibleBack
         handleBack={() => navigate("../select_golf_course")}
       />
-      <div>골프장 추가 테스트</div>
+      <Styled.Body></Styled.Body>
       <Button onClick={() => navigate("../make_golf_course_detail")}>
         다음
       </Button>
-    </div>
+    </Styled.Wrapper>
   );
+};
+
+const Styled = {
+  Wrapper: styled.div``,
+  Body: styled.div``,
 };
