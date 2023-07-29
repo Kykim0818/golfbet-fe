75d73300c24429c84ca5e4aa41274fd1f7a93394
Kim Ki Young <kykim818@naver.com>
2023-07-29 14:59:24 +0900

    골프장 추가 진행중

--- src/pages/MakeGame/MakeGolfCourse/MakeGolfCourse.tsx
+++ src/pages/MakeGame/MakeGolfCourse/MakeGolfCourse.tsx
@@ -1,10 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Button from "../../../components/Button";
+import Input from "../../../components/Input";
 import TitleAsset from "../../../components/TitleAsset";
+import { useRef } from "react";
+import { GameInfo } from "../MakeGame";
+
+type GolfCourseInfo = {
+  type: GameInfo["gameType"];
+  name: string;
+  location: string;
+  frontNineCourseName: string;
+  backNineCourseName: string;
+};
 
 export const MakeGolfCourse = () => {
   const navigate = useNavigate();
+  const golfCourseInfo = useRef<GolfCourseInfo>({
+    type: "field",
+    name: "",
+    location: "",
+    frontNineCourseName: "",
+    backNineCourseName: "",
+  });
 
   return (
     <Styled.Wrapper>
@@ -13,7 +31,40 @@ export const MakeGolfCourse = () => {
         visibleBack
         handleBack={() => navigate("../select_golf_course")}
       />
-      <Styled.Body></Styled.Body>
+      <Styled.Body>
+        <div>
+          <h5>골프장</h5>
+          <Input
+            placeholder="골프장명을 입력해주세요."
+            value={golfCourseInfo.current.name}
+            onChange={(e) => (golfCourseInfo.current.name = e.target.value)}
+          />
+        </div>
+        <div>
+          <h5>지역</h5>
+          <div>{golfCourseInfo.current.location}</div>
+        </div>
+        <div>
+          <h5>전반</h5>
+          <Input
+            placeholder="전반 코스 이름을 입력해주세요."
+            value={golfCourseInfo.current.frontNineCourseName}
+            onChange={(e) =>
+              (golfCourseInfo.current.frontNineCourseName = e.target.value)
+            }
+          />
+        </div>
+        <div>
+          <h5>후반</h5>
+          <Input
+            placeholder="후반 코스 이름을 입력해주세요."
+            value={golfCourseInfo.current.backNineCourseName}
+            onChange={(e) =>
+              (golfCourseInfo.current.backNineCourseName = e.target.value)
+            }
+          />
+        </div>
+      </Styled.Body>
       <Button onClick={() => navigate("../make_golf_course_detail")}>
         다음
       </Button>
--- src/pages/MakeGame/MakeGolfCourse/MakeGolfCourseDetail.tsx
+++ src/pages/MakeGame/MakeGolfCourse/MakeGolfCourseDetail.tsx
@@ -1,18 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../../../components/Button";
 import TitleAsset from "../../../components/TitleAsset";
+import styled from "styled-components";
 
 export const MakeGolfCourseDetail = () => {
   const navigate = useNavigate();
   return (
-    <div>
+    <Styled.Wrapper>
       <TitleAsset
         title="골프장 상세"
         visibleBack
         handleBack={() => navigate("../make_golf_course")}
       />
-      <div>골프장 상세</div>
+      <Styled.Body>골프장 상세</Styled.Body>
       <Button onClick={() => navigate("/make_game")}>추가 후 선택하기</Button>
-    </div>
+    </Styled.Wrapper>
   );
+};
+
+const Styled = {
+  Wrapper: styled.div``,
+  Body: styled.div``,
 };
