ae7954ff0b44fe241c425669969e5b2210380041
Kykim0818 <Kykim@seegene.com>
2023-08-21 17:17:00 +0900

    생성 전 수정화면에서 코스별 Par 수정 가능하게 구현
    - Par 수정 이후 생성시에만 변경된 정보로 Post 할 예정
    - 수정화면에서 돌아갈 시 기존 값 대로 복원

--- src/pages/MakeGame/SetupCheck/ParDetail.tsx
+++ src/pages/MakeGame/SetupCheck/ParDetail.tsx
@@ -7,29 +7,61 @@ type Props = {
   onChange?: (holeIndex: number, parCount: number) => void;
 };
 
-export const ParDetail = ({ holeIndex, parCount }: Props) => {
+export const ParDetail = ({ holeIndex, parCount, onChange }: Props) => {
   const [parCountState, setParCountState] = useState(parCount);
   const [parsOverFive, setParsOverFive] = useState(getParsOverFive(parCount));
 
   // TODO
-  const handleOnClick = (holeIndex: number, parCount: number) => {};
+  const handleOnClick = (holeIndex: number, parCount: number) => {
+    // state
+    setParCountState(parCount);
+
+    // 저장값 변경
+    onChange?.(holeIndex, parCount);
+  };
 
   // TODO
-  const handlePlusClick = () => {};
+  const handlePlusClick = () => {
+    const MAX_DEFAULT_PAR = 5;
+    // 기본 5 + 현재까지 추가된 par + 의 다음 1
+    setParsOverFive(getParsOverFive(MAX_DEFAULT_PAR + parsOverFive.length + 1));
+  };
   return (
     <S.Wrapper>
       <S.HoleIndex>{holeIndex}H</S.HoleIndex>
       <S.ParSection>
-        <S.Par isActive={parCountState === 3}>파3</S.Par>
-        <S.Par isActive={parCountState === 4}>파4</S.Par>
-        <S.Par isActive={parCountState === 5}>파5</S.Par>
-        {parsOverFive.length > 0 ? (
-          parsOverFive.map((par) => {
-            return <S.Par isActive={par === parCountState}>파{par}</S.Par>;
-          })
-        ) : (
-          <S.Par isActive={false}>+</S.Par>
-        )}
+        <S.Par
+          isActive={parCountState === 3}
+          onClick={() => handleOnClick(holeIndex, 3)}
+        >
+          파3
+        </S.Par>
+        <S.Par
+          isActive={parCountState === 4}
+          onClick={() => handleOnClick(holeIndex, 4)}
+        >
+          파4
+        </S.Par>
+        <S.Par
+          isActive={parCountState === 5}
+          onClick={() => handleOnClick(holeIndex, 5)}
+        >
+          파5
+        </S.Par>
+        {/* 6홀 이상 */}
+        {parsOverFive.map((par) => {
+          return (
+            <S.Par
+              isActive={par === parCountState}
+              onClick={() => handleOnClick(holeIndex, par)}
+            >
+              파{par}
+            </S.Par>
+          );
+        })}
+        <S.Par isActive={false} onClick={handlePlusClick}>
+          +
+        </S.Par>
       </S.ParSection>
     </S.Wrapper>
   );
@@ -62,13 +94,15 @@ const S = {
   ParSection: styled.div`
     display: flex;
     gap: 10px;
+    overflow: auto;
   `,
   Par: styled.div<{ isActive: boolean }>`
     display: flex;
     justify-content: center;
 
-    min-width: 40px;
+    min-width: 45px;
     min-height: 40px;
+    max-height: 40px;
 
     padding: 10px;
     border-radius: 15px;
@@ -90,5 +124,8 @@ const S = {
             color: var(--color-sub-text-grey, #bcbcbc);
             background-color: #f4f7fd;
           `}
+    &:hover {
+      cursor: pointer;
+    }
   `,
 };
