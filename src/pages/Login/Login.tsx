2c62c3afc53d82cf490edd81248ea6c6adbbf553
Kykim0818 <Kykim@seegene.com>
2023-10-19 13:22:03 +0900

    리다이렉트 URL 수정

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -6,7 +6,9 @@ import { LOGIN } from "../../service/login/constant";
 import { setCookie } from "../../utils/cookie";
 
 const REACT_APP_KAKAO_API = process.env.REACT_APP_KAKAO_API;
-export const REACT_APP_KAKAO_REDIRECT = `${process.env.PUBLIC_URL}/login`;
+export const REACT_APP_KAKAO_REDIRECT = process.env.PUBLIC_URL
+  ? `${process.env.REACT_KAKAO_REDIRECT_API}/login`
+  : "http://localhost:3000/login";
 
 export const Login = (props: {
   handleLogin: (accessToken: string, refreshToken: string) => void;
