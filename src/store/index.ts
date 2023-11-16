aeca53e35743560575f7cddc7229041927a7c927
Ki-Young <kykim8180@gmail.com>
2023-11-16 14:11:43 +0900

    feat:modal 관리용 slice 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
+import modalSlice from "./modal/modalSlice";
 import userSlice from "./user/userSlice";
 // ...
 
 export const store = configureStore({
   reducer: {
     users: userSlice,
+    modal: modalSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        // ignoredActions: [""],
+        // Ignore these field paths in all actions
+        ignoredActionPaths: ["payload.handleClose"],
+        // // Ignore these paths in the state
+        // ignoredPaths: ["items.dates"],
+      },
+    }),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
