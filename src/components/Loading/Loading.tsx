34c7c717eec11a0ae024f03b623640bdda686267
Ki-Young <kykim8180@gmail.com>
2023-11-23 13:36:25 +0900

    feat(loading):background 불투명으로 수정

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,21 +2,32 @@ import styled from "styled-components";
 
 export const Loading = () => {
   return (
-    <S.Background>
-      <S.Body>
-        <S.Img src={process.env.PUBLIC_URL + "/assets/gif/loading.gif"} />
-      </S.Body>
-    </S.Background>
+    <S.PageBackground>
+      <S.Background>
+        <S.Body>
+          <S.Img src={process.env.PUBLIC_URL + "/assets/gif/loading.gif"} />
+        </S.Body>
+      </S.Background>
+    </S.PageBackground>
   );
 };
 
 const S = {
+  PageBackground: styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: #f6f8fc;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  `,
   Background: styled.div`
     position: fixed;
     top: 0;
     left: 0;
     background-color: rgba(79, 77, 77, 0.61);
-    z-index: 3;
+    z-index: 4;
     width: 100%;
     height: 100%;
     overflow: hidden;
