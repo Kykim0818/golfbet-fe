983218adaec1cb45d3bc7e0371f1b27905c9acee
Ki-Young <kykim8180@gmail.com>
2024-01-22 19:10:56 +0900

    feat: 게임 진행 - 진행현황 탭 구현
    - 점수 전원 미 입력 시 처리
    - 일부 입력 시 처리
    
    - TODO: 점수 수정 구현 필요
    - TODO: css 수정 필요

--- src/pages/GameRoom/InGame/ProgressTab/ProgressBoard/ParBlock.tsx
+++ src/pages/GameRoom/InGame/ProgressTab/ProgressBoard/ParBlock.tsx
@@ -4,22 +4,30 @@ export const STATUS = {
   NOT_STARTED: "Not Started",
   IN_PROGRESS: "In Progress",
   COMPLETED: "Completed",
+  COMPLETED_FOCUS: "Completed clicked",
 } as const;
 
 type ParBlockProps = {
   status: (typeof STATUS)[keyof typeof STATUS];
-  holeIndex: number;
+  hole: number;
   parCount: number;
+  handleHoleClick: (hole: number) => void;
 };
 
 export const ParBlock = ({
-  status: state,
-  holeIndex,
+  status,
+  hole,
   parCount,
+  handleHoleClick,
 }: ParBlockProps) => {
   return (
-    <S.Wrapper variantStyle={BLOCK_TYPE[state]}>
-      <span className="parblock__holeindex">{holeIndex}H</span>
+    <S.Wrapper
+      variantStyle={BLOCK_TYPE[status]}
+      onClick={() => {
+        if (status !== STATUS.NOT_STARTED) handleHoleClick(hole);
+      }}
+    >
+      <span className="parblock__holeindex">{hole}H</span>
       <span className="parblock__parcount">{parCount}파</span>
     </S.Wrapper>
   );
@@ -55,7 +63,7 @@ const S = {
 
 const BLOCK_TYPE = {
   [STATUS.IN_PROGRESS]: css`
-    color: var(--color_main-dark, #008395);
+    color: var(--color-main-dark, #008395);
     background-color: #b0e6ed;
     border: 0.5px solid var(--color-main-dark, #008395);
   `,
@@ -68,5 +76,10 @@ const BLOCK_TYPE = {
     color: var(--color-sub-text-grey, #bcbcbc);
     background-color: #f4f7fd;
     border: 0.5px solid #f4f7fd;
+  `,
+  [STATUS.COMPLETED_FOCUS]: css`
+    color: var(--color-sub-text-grey, #bcbcbc);
+    background-color: var(--color-main, #009eb2);
+    border: 0.5px solid var(--color-main, #009eb2);
   `,
 };
---
+++ src/pages/GameRoom/InGame/ProgressTab/ProgressTab.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import styled, { css } from "styled-components";
+import Button from "../../../../components/Button";
+import { useAppSelector } from "../../../../hooks/redux";
+import { typo } from "../../../../styles/typo";
+import { GameInfo } from "../../../MakeGame/MakeGame";
+import { GameRoomUser } from "../../GameRoom";
+import ProgressBoard from "./ProgressBoard";
+import { ProgressPlayerRow } from "./ProgressPlayerRow";
+
+type ProgressTabProps = {
+  currentHole: number;
+  centerInfo: GameInfo["golfCenter"];
+  players: GameRoomUser[];
+  handleOpenEnterScore: () => Promise<void>;
+};
+export const ProgressTab = ({
+  currentHole,
+  centerInfo,
+  players,
+  handleOpenEnterScore,
+}: ProgressTabProps) => {
+  const [selectHole, setSelectHole] = useState(currentHole);
+  const userInfo = useAppSelector((state) => state.users.userInfo);
+  const isNoBody = players.every(
+    (player) => player.holeScores[selectHole - 1] === 999
+  );
+
+  const handleHoleClick = (hole: number) => {
+    setSelectHole(hole);
+  };
+
+  useEffect(() => {
+    setSelectHole(currentHole);
+  }, [currentHole]);
+
+  return (
+    <S.Wrapper>
+      <ProgressBoard
+        selectHole={selectHole}
+        currentHole={currentHole}
+        centerInfo={centerInfo}
+        handleHoleClick={handleHoleClick}
+      />
+      {/* 플레이어 판 */}
+      <S.Players isNobody={isNoBody}>
+        {isNoBody ? (
+          <span>스코어를 입력해주세요.</span>
+        ) : (
+          players.map((player) => {
+            return (
+              <ProgressPlayerRow
+                key={player.userId}
+                selectHole={selectHole}
+                player={player}
+                isSelf={player.userId === userInfo.userId}
+              />
+            );
+          })
+        )}
+      </S.Players>
+      <S.Footer>
+        {selectHole === currentHole ? (
+          <Button onClick={handleOpenEnterScore}>+스코어 입력하기</Button>
+        ) : (
+          <Button
+            disabled
+            onClick={() => {
+              alert("수정하기 미 구현");
+            }}
+          >
+            +스코어 수정하기
+          </Button>
+        )}
+      </S.Footer>
+    </S.Wrapper>
+  );
+};
+
+const S = {
+  Wrapper: styled.div`
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column;
+  `,
+  Players: styled.div<{ isNobody: boolean }>`
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    gap: 15px;
+    margin-top: 32px;
+
+    ${(props) =>
+      props.isNobody &&
+      css`
+        justify-content: center;
+        align-items: center;
+        span {
+          ${typo.s14w400}
+          color:#A7A7A7;
+        }
+      `}
+  `,
+  Footer: styled.footer`
+    display: flex;
+    padding-bottom: 20px;
+  `,
+};
