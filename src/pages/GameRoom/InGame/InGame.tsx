01efeccc904d32ee3d7486cde350416ad4313c28
Ki-Young <kykim8180@gmail.com>
2024-02-16 17:19:55 +0900

    feat: 게임 종료 팝업 추가
    - 뒤로가기 불가

--- src/pages/GameRoom/InGame/InGame.tsx
+++ src/pages/GameRoom/InGame/InGame.tsx
@@ -108,7 +108,7 @@ export const InGame = ({
       setCanEnterScore(gameId ?? "", "");
     }
   }, [setCanEnterScore, gameId, userInfo.userId]);
-
+  // 전반 종료
   useStrictModeEffectOnce(() => {
     if (currentHole === BACK_NINE_START_HOLE && isBackNineStart === false) {
       openModal({
